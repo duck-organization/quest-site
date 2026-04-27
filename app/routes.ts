5e399a3b9e978500a4f961fa4c8f45870b6c19cb
lunarash0 <eamonhwatson@icloud.com>
2026-04-28 01:00:19 +0200

    update

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,7 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, index, route, layout, prefix } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+    index("routes/home.tsx"),
+    route("bot/invite", "routes/bot/invite.tsx"),
+    route("*", "routes/$.tsx"),
+] satisfies RouteConfig;
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,21 @@
+import { defineConfig } from "vite";
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
+import mdx from "@mdx-js/rollup";
+import remarkGfm from "remark-gfm";
+import rehypeSlug from "rehype-slug";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter()],
-  resolve: {
-    tsconfigPaths: true,
-  },
+    plugins: [
+        {
+            enforce: 'pre',
+            ...mdx({
+                remarkPlugins: [remarkGfm],
+                rehypePlugins: [rehypeSlug, rehypeAutolinkHeadings],
+            }),
+        },
+        reactRouter(),
+        tailwindcss(),
+    ],
 });
