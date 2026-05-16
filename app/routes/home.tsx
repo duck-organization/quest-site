ed99e4a34e60910fe51a024fbbe408b8bcb9b079
unbravechimp <eamonhwatson@icloud.com>
2026-05-16 03:00:23 +0200

    fix: replace image with longer

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -4,7 +4,7 @@ import { Landing } from "./landing/landing";
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "Duck Organization" },
-    { property: "og:image", content: "https://duckorg.com/logoBanner.png" },
+    { property: "og:image", content: "https://duckorg.com/largeDuckBanner.png" },
     { name: "description", content: "The one and only Duck Organization." },
   ];
 }
